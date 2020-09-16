0974bcfefd646a39634d9614246f95e8d0d9ceed
Vagus-art <agustin.favo@gmail.com>
2020-09-16 17:32:50 -0300

    solucionado bug de string en valores numericos

--- app/Services/StatsService.php
+++ app/Services/StatsService.php
@@ -33,13 +33,14 @@ class StatsService implements StatsServiceInterface
 
     public function getStats()
     {
+        //a veces las consultas retornan strings en vez de numeros, por eso le agregué el typecasting feo este
         $pendingOrdersCount = $this->ordersRepo->getPendingOrdersCount();
-        $expensesSum = $this->expensesRepo->getExpensesSum();
-        $transactionsInSum = $this->transactionsRepo->getTransactionsSum('b');
-        $transactionsOutSum = $this->transactionsRepo->getTransactionsSum('a');
+        $expensesSum = (float)$this->expensesRepo->getExpensesSum();
+        $transactionsInSum = (float)$this->transactionsRepo->getTransactionsSum('b');
+        $transactionsOutSum = (float)$this->transactionsRepo->getTransactionsSum('a');
         $benefits = $transactionsInSum - $transactionsOutSum - $expensesSum;
-        $contactsDebt = $this->contactsRepo->getContactsDebt();
-        $ownDebt = $this->contactsRepo->getOwnDebt();
+        $contactsDebt = (float)$this->contactsRepo->getContactsDebt();
+        $ownDebt = (float)$this->contactsRepo->getOwnDebt();
         return ['pending_orders'=>$pendingOrdersCount, 'expenses_sum'=>$expensesSum, 'transactions_in'=>$transactionsInSum, 'transactions_out'=>$transactionsOutSum, 'benefits'=>$benefits, 'contacts_debt'=>$contactsDebt, 'own_debt'=>$ownDebt];
     }
 
